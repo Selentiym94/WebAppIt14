7c208db0d9746d67e047fc080a21d1d9e3a334a1
Student <Student@KVANT-29>
2024-03-16 13:08:51 +0300

    клианет.начало

--- WebApplication.Logic/Processors/PlaceholderClient.cs
+++ WebApplication.Logic/Processors/PlaceholderClient.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using WebApplication.Logic.Interfaces;
 
@@ -10,9 +11,33 @@ namespace WebApplication.Logic.Processors
 {
     public class PlaceholderClient : IPlaceholderClient
     {
-        public User GetUser(string name)
+        private readonly string _baseUrl;
+
+        public PlaceholderClient(string baseUrl)
         {
-            throw new NotImplementedException();
+            _baseUrl = baseUrl;
+        }
+        public async Task<List<User>> GetUser(string name)
+        {
+            return await GetData<User>("users");
+        }
+
+        private async Task<List<TData>> GetData<TData>(string path)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.RequestUri = new Uri($"{_baseUrl}/{path}");
+                message.Method = HttpMethod.Get;
+
+                HttpResponseMessage response = await client.SendAsync(message);
+                if (response.IsSuccessStatusCode)
+                {
+                    string dataString = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<List<TData>>(dataString);
+                }
+                return new List<TData>();
+            }
         }
     }
 }
