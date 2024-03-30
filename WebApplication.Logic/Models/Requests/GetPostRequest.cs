8f8580d978aae6469b81dc479598a6e8af24e5f2
ghgtv <zahtr@mail.ru>
2024-03-30 12:14:13 +0300

    запрос для постов

---
+++ WebApplication.Logic/Models/Requests/GetPostRequest.cs
@@ -0,0 +1,22 @@
+﻿using WebApplication.Logic.Interfaces;
+
+namespace WebApplication.Logic.Models.Requests
+{
+    public class GetPostRequest : ITypeCodeRequest
+    {
+        public GetPostRequest(int userId)
+        {
+                UsertId = userId;
+        }
+        public int UsertId { get; set; }
+        public string GetRequestsParams()
+        {
+            string result = "post";
+            if (UsertId > 0)
+            {
+                result += $"?userId={UsertId}";
+            }
+            return result;
+        }
+    }
+}
